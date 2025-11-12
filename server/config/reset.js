7350def470fae30559a40cd6df6abe91e97444d0
ongarrick <75192692+norellic@users.noreply.github.com>
2025-11-11 23:17:49 -0500

    Document dbdiagram.io database structure in reset.js for reference for table queries
    Added database structure comments for users, habits, tags, and stickers.

--- server/config/reset.js
+++ server/config/reset.js
@@ -2,4 +2,55 @@
 //import dotenv.js
 //import data.js
 
-//create tabel queries
+//create table queries
+
+/*
+dbdiagram.io structure:
+
+Table users {
+  id integer [primary key]
+  username varchar
+  password varchar
+  points integer
+}
+
+Table habits {
+  id integer [primary key]
+  user_id integer [not null]
+  title varchar
+  description text [note: 'Description of the habit']
+}
+
+Table tags {
+  id integer [primary key]
+  name varchar
+}
+
+Table habitTags { //join table
+  habit_id integer [not null]
+  tag_id integer [not null]
+}
+
+Table stickers {
+  id integer [primary key]
+  name varchar
+  price integer
+  image_url url
+}
+
+Table userStickers {
+  user_id integer [not null]
+  sticker_id integer [not null]
+}
+
+Ref user_habits: habits.user_id > users.id // many-to-one
+
+Ref link_habit_id_to_habitTag: habitTags.habit_id > habits.id
+
+Ref link_tag_id_to_habitTag: habitTags.tag_id > tags.id
+
+Ref link_user_id_to_userStickers: userStickers.user_id > users.id
+
+Ref link_sticker_id_to_userStickers: userStickers.sticker_id > stickers.id
+
+*/
